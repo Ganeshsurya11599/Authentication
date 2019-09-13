5d05e2641b2aebc936ed6e6973e80b7eaf123cc6
Ganeshsurya11599 <a.surya11599@gnail.com>
2019-09-13 03:05:06 -0700

    auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { environment } from 'src/environments/environment.prod';
+
+var config = {
+  apiKey: "AIzaSyC3QRqF933S_GC-TQiNPwi990JNDb2Zdj0",
+  authDomain: "authentication-39380.firebaseapp.com",
+  databaseURL: "https://authentication-39380.firebaseio.com",
+  projectId: "authentication-39380",
+  storageBucket: "",
+  messagingSenderId: "1085569220351",
+  appId: "1:1085569220351:web:b01affa8adc779bb80d6ca"
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    FormsModule,
+    AngularFireModule.initializeApp(config),
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
